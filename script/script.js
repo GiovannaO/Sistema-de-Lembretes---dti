2daa53360dac7693df764036c31ea98b696b5dc8
GiovannaO <91629770+GiovannaO@users.noreply.github.com>
2022-09-09 11:54:27 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,44 +1,27 @@
+var botaoCriar = document.querySelector("#criar");
+//criação do evento do botão
+if(botaoCriar){
+    botaoCriar.addEventListener("click", () =>{
+        validarFormulario();
+    });
+}
+
 var d = new Date();
 var dia = d.getDate();
 var mes = d.getMonth() + 1;
 var ano = d.getFullYear();
 
-//função para adicionar data na lista de lembretes
-function adicionarData(dataCompleta){
-
-    var listaData = [];
-
-    listaData.push(dataCompleta);
-
-}
-
-//função para adicionar nome de lembrete na lista
-function adicionarNome(nome){
-
-    var listaNome = [];
-
-    listaNome.push(nome);
-}
-
-/*remover item da lista, quando o botão excluir é pressionado
-function removerItem(nome, listaData, listaNome){
-
-    listaNome.splice(listaNome.indexOf(nome), 1);
-
-    //caso a lista de nomes do lembrete fique vazia, a lista de datas também deve ser removida
-    if(listaNome == ""){
-        listaData.splice(listaData.indexOf(data), 1);
-    }
-
-}*/
 
 //validação do formulário de criação de lembretes
 function validarFormulario(){
 
-    var nome = document.getElementById("nome");
-    var data = document.getElementById("data");
+    var nome = document.querySelector("#nome");
+    var data = document.querySelector("#data");
 
+    //atribuindo os valores dos elementos html á variáveis
+    var nomeSelecionado = nome.value;
     var dataSelecionada = new Date(data.value);
+
     //armazenando a data em variáveis diferentes
     var diaSelecionado = dataSelecionada.getDate() + 1;
     var mesSelecionado = dataSelecionada.getMonth() + 1;
@@ -46,23 +29,11 @@ function validarFormulario(){
 
     var dataCompleta = diaSelecionado + "/" + mesSelecionado + "/" + anoSelecionado;
 
-    //funções que validam a data e o nome dos lembretes separadamente
-    validarNome(nome);
-    validarData(data, diaSelecionado, mesSelecionado, anoSelecionado);
-
-}
-
-//função para validar o nome do lembrete
-function validarNome(nome){
-    //verificar se o campo nome está vazio
-    if(nome.value == ""){
+     //verificar se o campo nome está vazio
+     if(nomeSelecionado == ""){
         alert("Nome do lembrete não informado");
         nome.focus();
     }
-}
-
-//função para validar a data do lembrete
-function validarData(data, dataCompleta, anoSelecionado, mesSelecionado, diaSelecionado){
 
     //se a data não for selecionada
     if(data.value == ''){
@@ -77,25 +48,49 @@ function validarData(data, dataCompleta, anoSelecionado, mesSelecionado, diaSele
             data.focus();
         }else if(anoSelecionado == ano && (mesSelecionado == mes && diaSelecionado > dia) || (mesSelecionado > mes)){
             //a data é posterior ao dia atual, porém no mesmo ano e/ou mesmo mês
-            criarLembrete(nome, dataCompleta);
+            criarLembrete(nomeSelecionado, dataCompleta);
         }else{
             //a data é posterior ao dia atual
-            criarLembrete(nome, dataCompleta);
+            criarLembrete(nomeSelecionado, dataCompleta);
         }
 
     }
 
 }
 
-function criarLembrete(nome, dataCompleta){
+//função para criar lembretes
+function criarLembrete(nomeSelecionado, dataCompleta){
 
-    var adicionar1 = adicionarData(dataCompleta);
-    var adicionar2 = adicionarNome(nome);
-    
-    var dataAdicionada = adicionar1.value;
-    var nomeAdicionado = adicionar2.value;
+    //variáveis para criar tags html
+    var ul = document.createElement("ul");
+    var li = document.createElement("li");
 
-    document.getElementById(listaLembretes).appendChild(dataAdicionada);
-    document.getElementById(listaLembretes).appendChild(nomeAdicionado);
+    //variáveis para indicar o que será escrito dentro do bloco de cada tags
+    var textoData = document.createTextNode(dataCompleta);
+    var textoNome = document.createTextNode(nomeSelecionado);
+
+    ul.appendChild(textoData);
+    li.appendChild(textoNome);
+
+    if(textoData == dataCompleta){
+        document.getElementById("listaLembretes").appendChild(li);
+    }else{
+        document.getElementById("listaLembretes").appendChild(ul);
+        document.getElementById("listaLembretes").appendChild(li);
+    }
 
 }
+
+/*remover item da lista, quando o botão excluir é pressionado
+function removerItem(nomeSelecionado){
+    listaNome = [];
+    listaNome = nomeSelecionado;
+
+    listaNome.splice(listaNome.indexOf(nomeSelecionado), 1);
+
+    //caso a lista de nomes do lembrete fique vazia, a lista de datas também deve ser removida
+    if(listaNome == ""){
+        listaData.splice(listaData.indexOf(dataCompleta), 1);
+    }
+
+}*/
